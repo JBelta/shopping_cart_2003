a8fe9a0bc83dba997ccbdbfa9dfc42f83aa60183
James Belta <james.belta@gmail.com>
2020-03-25 10:18:54 -0600

    Edit semicolon after pry on shopping_cart_test.rb. Wasted 35- 40 min ruby error message was expecteing 'end'.

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,7 +6,7 @@ require './lib/shopping_cart'
 class ShoppingCartTest < Minitest::Test
   def test_it_exists
     cart = ShoppingCart.new("King Soopers", "30items")
-    require 'pry'' binding.pry
+    require 'pry'; binding.pry
     assert_instance_of ShoppingCart, cart
   end
 end
